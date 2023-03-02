f326213fc4eac2364089def8d9cc5be6f27fd1fc
Ugglr <carl.igelstrom@gmail.com>
2023-03-02 12:01:49 +0800

    grid and related components implemented

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.1337co.de",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
